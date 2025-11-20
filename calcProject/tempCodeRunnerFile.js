a3e398a0eadfdacaacb46510f1137cabdd2524c1
harsh8520 <harshasoriya258@gmail.com>
2025-11-20 11:33:14 +0530

    Cleaned repo — removed unrelated projects

--- calcProject/tempCodeRunnerFile.js
+++
@@ -1,6 +0,0 @@
-val = '0'
-// let btn = Array.from(document.querySelectorAll('.btn'))
-// let outputScreen = document.querySelector('.output')
-// let exp = ''
-// let outputValue = ''
-// const maxLength = 12
